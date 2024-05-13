ad1db99bceb8aba01baa0a86ffb4aa4b8c42ebf0
shhriiii <149945420+shhriiii@users.noreply.github.com>
2024-05-13 23:31:17 +0530

    Add files via upload

---
+++ assgmnt 7/q1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+int x, y;
+cin >> x >> y;
+int *ptrX, *ptrY;
+ptrX = &x;
+ptrY = &y;
+int ans;
+int *ptrAns = &ans;
+*ptrAns = (*ptrX) * (*ptrY);
+cout<< *ptrAns << endl;
+return 0;
+}
